zerocurrencycoin/Zero

Reset saplingAccountCounter for wallets with different original bip44CoinType

src/wallet/wallet.h
@@ -1179,7 +1179,7 @@ public:
       * Sapling ZKeys
       */
     //! Generates new Sapling key
-    libzcash::SaplingPaymentAddress GenerateNewSaplingZKey();
+    libzcash::SaplingPaymentAddress GenerateNewSaplingZKey(bool resetCounter = false);
     //! Adds Sapling spending key to the store, and saves it to disk
     bool AddSaplingZKey(
         const libzcash::SaplingExtendedSpendingKey &key,

zelcash/zelcash

src/wallet/wallet.h
@@ -1110,7 +1110,7 @@ public:
       * Sapling ZKeys
       */
     //! Generates new Sapling key
-    libzelcash::SaplingPaymentAddress GenerateNewSaplingZKey();
+    libzelcash::SaplingPaymentAddress GenerateNewSaplingZKey(bool resetCounter = false);
     //! Adds Sapling spending key to the store, and saves it to disk
     bool AddSaplingZKey(
         const libzelcash::SaplingExtendedSpendingKey &key,